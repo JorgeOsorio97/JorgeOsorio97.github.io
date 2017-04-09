c4ff900faa499a0291e50565fff08037a26f9b5d
Jorge Osorio <jorge.valdez.osorio@gmail.com>
2017-04-09 11:42:31 -0700

    Modificar zoom del maps

--- js/maps.js
+++ js/maps.js
@@ -1,12 +1,11 @@
 function initMap() {
      var uluru = {lat: 19.56236400564298, lng: -99.25781900000004};
      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 8,
         center: uluru
       });
      var marker = new google.maps.Marker({
          position: uluru,
          map: map
      });
- }
- 
+ s}
